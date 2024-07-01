52c87ed561cded394b10925370c5a7ce603c1fb2
Denis Nazarovs <denis.nazarovs@va.lv>
2024-07-01 16:35:23 +0300

    WIP: model scaling on resize, orientation detection

--- main.js
+++ main.js
@@ -11,8 +11,11 @@ if (!WebGL.isWebGL2Available()) {
 
 // Scene setup
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xf2f2f2);
 const canvas = document.querySelector('#c');
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+let aspectRatio = window.innerWidth / window.innerHeight;
 
 camera.position.z = 5;
 const light = new THREE.AmbientLight(0x404040, 50);
@@ -29,15 +32,28 @@ let popup;
 
 loader.load('/macosPopup.glb', function (gltf) {
     popup = gltf.scene;
-    popup.rotation.y = 0.5;
-    popup.rotation.x = 0.1;
-    popup.position.z = -3;
-    popup.position.x = -3.5;
-    popup.position.y = 3;
-    popup.scale.set(0.75, 0.75, 0.75);
+    // popup.rotation.y = 0.5;
+    // popup.rotation.x = 0.1;
+    // popup.position.z = -3;
+
+    console.log(aspectRatio)
+
+    popup.position.x = 0
+    popup.position.y = 0;
+    popup.scale.set(aspectRatio, aspectRatio, aspectRatio);
     scene.add(popup);
 }, undefined, function (error) {
     console.error(error);
+});
+
+screen.orientation.addEventListener("change", function (e) {
+    if (resizeRendererToDisplaySize(renderer)) {
+        aspectRatio = window.innerWidth / window.innerHeight
+        const canvas = renderer.domElement;
+        popup.scale.set(aspectRatio, aspectRatio, aspectRatio);
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
 });
 
 // Handles resizing the window, and hi-dpi displays
@@ -60,12 +76,8 @@ function render(time) {
     if (resizeRendererToDisplaySize(renderer)) {
         const canvas = renderer.domElement;
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        popup.scale.set(aspectRatio, aspectRatio, aspectRatio);
         camera.updateProjectionMatrix();
-    }
-
-    // Rotate object
-    if (popup) {
-        popup.rotation.x += 0.01;
     }
 
     renderer.render(scene, camera);
@@ -77,7 +89,9 @@ requestAnimationFrame(render);
 
 // Handle window resize
 window.addEventListener('resize', () => {
+    aspectRatio = window.innerWidth / window.innerHeight
     resizeRendererToDisplaySize(renderer);
     camera.aspect = window.innerWidth / window.innerHeight;
+    popup.scale.set(aspectRatio, aspectRatio, aspectRatio);
     camera.updateProjectionMatrix();
 });
